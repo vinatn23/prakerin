7ca57d799029c8242c64cc9d797bb21a6879c269
vinatn23 <vinatn26@gmail.com>
2021-02-27 09:22:39 +0700

    keberapa kali

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,5 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,13 +9,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-/*Route::get('test', function () {
-    return view('layouts.master');
-});
-*/
-
 Route::get('admin',[App\Http\Controllers\HomeController::class, 'admin']);
-
 
 use App\Http\Controllers\ProvinsiController;
 Route::resource('admin/provinsi', ProvinsiController::class);
@@ -48,4 +30,4 @@ use App\Http\Controllers\Kasus2Controller;
 Route::resource('admin/kasus2', Kasus2Controller::class);
 
 use App\Http\Controllers\FrontendController;
-Route::resource('index', FrontendController::class);
+Route::resource('/', FrontendController::class);
